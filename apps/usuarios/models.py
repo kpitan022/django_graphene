e7c3be4778bc9fbd1edd9afde97367c05cd326cc
kpitan022 <kpitan022@gmail.com>
2021-12-13 01:02:34 -0300

    a ver ahora

--- apps/usuarios/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models):
-    name=models.CharField(max_length=50)
-    age=models.IntegerField()
---
+++ students/schema.py
@@ -0,0 +1,11 @@
+
+import graphene
+
+from students.apps.usuarios import schema
+
+
+class Query(schema.Query, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query)
